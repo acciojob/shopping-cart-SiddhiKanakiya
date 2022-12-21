e3e725ee8e3c1144156437e47dc1f7eb2dc1a465
SiddhiKanakiya <siddhikanakiya@gmail.com>
2022-12-21 18:39:35 +0530

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+let tableBody = document.getElementsByTagName("tbody")[0];
+let addBtn = document.getElementsByTagName("button")[0];
+let totalmoney = 0;
+let money = document.getElementById("total");
+
+
+function updateItem(){
+
+    let item = document.getElementById("item-name-input").value;
+    let price = Number(document.getElementById("item-price-input").value);
+    totalmoney +=price;
+    money.innerText = "Grand Money:" + totalmoney;
+
+    let createRow = document.createElement("tr");
+    let createitem = document.createElement("td");
+    let createrprice = document.createElement("td");
+    createitem.innerText = item;
+    createrprice.innerText = price;
+
+    createRow.append(createitem);
+    createRow.append(createrprice);
+    
+
+    tableBody.append(createRow);
+
+    document.getElementById("item-name-input").value = "";
+    document.getElementById("item-price-input").value = "";
+
+
+}    
+
+addBtn.onclick = updateItem;
